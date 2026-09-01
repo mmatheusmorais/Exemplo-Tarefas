fd08bd7051400bf6fec8323ef195399e69756696
Mmatheusmorais <matheusmarcelinodemorais@gmail.com>
2026-09-01 11:54:06 -0300

    Feat: funcionalidades de tarefas

--- src/components/Tarefa.jsx
+++ src/components/Tarefa.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react"
+import '../css/estilo.css'
+import Contador from './Contador'
 
 const Tarefa = () => {
 
@@ -15,10 +17,55 @@ const Tarefa = () => {
         localStorage.setItem("item-tarefa",JSON.stringify(tarefas))
     },[tarefas])
 
-    return (
-        <>
+    //FUNÇÃO ADICIONAR TAREFA - "e" é um evento poe ser encontrado com "e" ou "event"
+    const AdicionarTarefa = (e)=>{
+        //Previne que a página se recarregeue automaticamente
+        e.preventDefault();
+        //Valida se o campo estiver vazio
+        if(!campo.trim()) return;
 
-        </>
+        //novo objeto
+        const novaTarefa={
+            id: Date.now(),
+            texto:campo,
+        }
+        setTarefas([...tarefas,novaTarefa]);
+        setCampo('');
+    }
+    const RemoverTarefa=(id)=>{
+        //VERIFICA SE O ID DA TAREFA ATUAL É DIFERENTE DO ID QUE DESEJA APAGAR
+        //SE O ID FOR IGUAL(TAREFA QUE DESEJA APAGAR) A CONDIÇÃO RETORNA FALSO
+        //E O ITEM É EXCLUIDO
+        const apagarTarefa = tarefas.filter((tarefa)=> tarefa.id !== id);
+        setTarefas(apagarTarefa);
+    }
+    return (
+        <div className="todo-container">
+            <h1>Minha Lista de Tarefas</h1>
+            <form onSubmit={AdicionarTarefa}>
+                <input
+                 type="text"
+                 value={campo} 
+                 onChange={(e)=>setCampo(e.target.value)}
+                 placeholder="Digite sua tarefa"
+                 className="todo-input"
+                 />
+                 <button type="submit">Adicionar</button>
+            </form>
+
+            <ul>
+                {tarefas.map((tarefa)=>(
+                    <li key={tarefa.id}>
+                        <span>{tarefa.texto}</span>
+                        <button onClick={()=>RemoverTarefa(tarefa.id)}>Excluir</button>
+                    </li>
+                ))}
+            </ul>
+            {/* COMPARA SE NÃO TIBER TAREFAS DEIXA A MENSAGEM NENHUMA TAREFA SALVA */}
+            {tarefas.lenght === 0 && <P>Nenhuma tarefa Salva</P>}
+            <Contador></Contador>
+
+        </div>
     )
 }
 
